7d5f1e1fee154f0cc3bf99d36258c39b54fd7ff9
Tazwar <142654711+Fahim-instaWebWorks@users.noreply.github.com>
2023-11-20 10:32:58 +0600

    mongose and nodemon installed

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const req = require('express/lib/request')
+const res = require('express/lib/response')
 const app = express()
 const port = 3000
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
+///routes///
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/blog',(req,res)=>{
+    res.send('hello im from blog and my name is fahim')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
