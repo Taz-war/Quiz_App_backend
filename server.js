c1d02f35d3730c35567e53fb8e2a3dcccef36e2f
Tazwar <142654711+Fahim-instaWebWorks@users.noreply.github.com>
2023-11-21 13:52:22 +0600

    frontend connected

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-const express = require('express')
-const req = require('express/lib/request')
-const res = require('express/lib/response')
-const cors= require('cors')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
 
 ///midleware///
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 ///mongo db user name & password///
 //userName : fahimtazwer
 //password : ALETgHxkxEf2sl8B
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://fahimtazwer:ALETgHxkxEf2sl8B@fahim1.p8agypj.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://fahimtazwer:ALETgHxkxEf2sl8B@fahim1.p8agypj.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,33 +23,53 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database = client.db("QuizDB");
+    const QuestionCollection = database.collection("Questions");
+    // const QuestionCollection =await client.db("admin").collections("Questions");
+
+    ///get request///
+    app.get('/questionSet',async(req,res)=>{
+      const cursor = QuestionCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    ///post request////
+    app.post("/questionSet", async (req, res) => {
+      const question = req.body;
+      console.log("new question created");
+      const result = await QuestionCollection.insertOne(question);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
 ///routes///
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/blog',(req,res)=>{
-    res.send('hello im from blog')
-})
+app.get("/blog", (req, res) => {
+  res.send("hello im from blog");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
