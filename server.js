5b5fe96c6d3ea551831120a87a795b1072e36cc0
Tazwar <142654711+Fahim-instaWebWorks@users.noreply.github.com>
2023-12-11 13:47:48 +0600

    socket connection

--- server.js
+++ server.js
@@ -4,11 +4,43 @@ const res = require("express/lib/response");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+const http = require('http');
+const socket = require('socket.io')
 const port = process.env.PORT || 5000;
 
 ///midleware///
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:4000' }));
 app.use(express.json());
+const server = http.createServer(app);
+// const io = socketIo(server);
+var io = socket(server, {
+  cors: {
+    origin: "http://localhost:4000",
+    // methods: ["GET", "POST"]
+  }
+})
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  // Joining a room
+  socket.on('joinRoom', (room) => {
+    socket.join(room);
+    console.log(`A user joined room: ${room}`);
+  });
+
+  // Receiving a message and broadcasting it to the room
+  socket.on('sendMessage', ({ room, message }) => {
+    io.to(room).emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log('Server listening on port 5000');
+});
 
 ///mongo db user name & password///
 //userName : fahimtazwer
@@ -166,14 +198,23 @@ async function run() {
 run().catch(console.dir);
 
 ///routes///
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.get("/blog", (req, res) => {
+//   res.send("hello im from blog");
+// });
+
+io.on('connection', (socket) => {
+  socket.on('connectQuiz',(data)=>{
+    console.log(data)
+    console.log('New client connected',socket.id);
+  })
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
 });
 
-app.get("/blog", (req, res) => {
-  res.send("hello im from blog");
-});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
